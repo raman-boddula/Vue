a47bcb49edf7cf812e942e492044561edb919d7b
Raman <raman.boddula@fraazo.com>
2022-09-26 16:05:05 +0530

    basic

---
+++ vue-task-tracker/src/components/Counter.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>Counter : {{counter}}</div>
+    <button class="btn" @click="onChange(1)">Increment</button>
+    <button class="btn" @click="onChange(-1)">Decrement</button>
+    <button class="btn" @dblclick="multiply">Multiply by 5</button>
+</template>
+<script>
+export default {
+    name: "counter-item",
+    data() {
+        return {
+            counter : 0
+        }
+    },
+    methods : {
+        onIncrement() {
+            this.counter = this.counter + 1
+        },
+        onDecrement() {
+            this.counter = this.counter - 1;   
+        },
+        onChange(value) {
+            this.counter = this.counter + value
+        },
+        multiply() {
+            this.counter = this.counter * 5
+        }
+    }
+}
+</script>
---
+++ vue-task-tracker/src/components/Todos.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <h3>Todo List</h3>
+    <input v-model="inputValue" type="text" name='inputValue'  placeholder="please add a todo"/>
+    <button class="btn" @click="addTodo">Add</button>
+    {{inputValue}}
+  </div>
+  <div :key="todo.id"  v-for="todo in todoList">
+        <p>{{todo.id}} - {{todo.title}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'TodosItem',
+    data() {
+        return {
+            todoList:[],
+            inputValue:'',
+            counter:0
+        }
+    },
+    methods: {
+        addTodo() {
+            if (!this.inputValue) {
+                alert('please add a input value');
+                return
+            }
+            this.todoList.push({ id: this.counter, title: this.inputValue });
+            this.counter++;
+            this.inputValue=''
+        }
+    }
+}
+</script>
+
+<style>
+input{
+    padding: 12px 18px;
+}
+</style>
